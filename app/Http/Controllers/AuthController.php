fefaf36b7016f5913fd0724edff427cc2cd25b56
Noor Alizah <4340264-nooralizahar@users.noreply.gitlab.com>
2021-06-02 18:45:20 +0700

    update form pengajuan

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -140,6 +140,8 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
+            'status' => 'ok',
+            'user' => Auth::guard()->user(),
             'expires_in' => auth('api')->factory()->getTTL() * 60
         ]);
     }
